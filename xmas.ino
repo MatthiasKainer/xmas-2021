29cd94d877da55ec20a2871601cef0ffd3f2cfed
Matthias Kainer <mail@matthias-kainer.de>
2021-12-22 17:51:13 +0100

    Added RGB LEDs

--- xmas.ino
+++ xmas.ino
@@ -1,6 +1,11 @@
 #include "stille-nacht.h" 
 
 #define DOORBELL 2
+#define LED_CYCLE_1 10
+#define LED_CYCLE_2 5
+#define RGB_R 6
+#define RGB_G 7
+#define RGB_B 8
 
 
 void dimLED(uint8_t led, uint8_t interval, uint32_t delay)
@@ -9,10 +14,20 @@ void dimLED(uint8_t led, uint8_t interval, uint32_t delay)
   analogWrite(led, time);
 }
 
+void rbgLED(uint8_t red, uint8_t green, uint8_t blue) {
+  unsigned int time = millis() % 4800;
+  digitalWrite(red, time > 700 && time < 2800 ? HIGH : LOW);
+  digitalWrite(green, time > 2100 && time < 4200 ? HIGH : LOW);
+  digitalWrite(blue, time < 1400 || time > 3500 ? HIGH : LOW);
+}
+
 void setup()
 {
   Serial.begin(9600); 
-  pinMode(DOORBELL, INPUT_PULLUP);
+  pinMode(DOORBELL, INPUT_PULLUP); 
+  pinMode(RGB_B, OUTPUT);
+  pinMode(RGB_R, OUTPUT);
+  pinMode(RGB_G, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(DOORBELL), ringDoorbell, RISING);
 }
 
@@ -23,6 +38,7 @@ void ringDoorbell() {
 void loop()
 {
   async_play();
-  dimLED(10, 255, 254);
-  dimLED(5, 255, 127);
+  dimLED(LED_CYCLE_1, 255, 254);
+  dimLED(LED_CYCLE_2, 255, 127);
+  rbgLED(RGB_R, RGB_G, RGB_B);
 }
